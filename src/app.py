ddd24548dffc179a506ddedb73c0e4a89ef6821a
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-26 13:28:57 -0300

    Implementando Websockets na Aplicação

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
-import sys
 import os
+import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
+from flask_socketio import SocketIO
 from datetime import datetime, timedelta
 from flask import Flask, jsonify, request, send_file, abort, render_template
+
+from payments.pix import Pix
 from repository.database import db
 from db_models.payment import Payment
-from payments.pix import Pix
 
 app = Flask(__name__)
 
@@ -21,6 +23,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(instance_path,
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_payment_pix():
@@ -56,7 +59,6 @@ def get_image(file_name):
   file_path = os.path.join(os.getcwd(), "src", "static", "img", f"{file_name}.png")
   
   if not os.path.exists(file_path):
-    print(f"Arquivo não encontrado: {file_path}")  # Para debugar
     abort(404, description="Arquivo não encontrado")
   
   return send_file(file_path, mimetype='image/png')
@@ -64,7 +66,6 @@ def get_image(file_name):
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def confirmation_pix():
   return jsonify({"message": "The payment has been confirmed"})
-
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
@@ -82,7 +83,11 @@ def payment_pix_page(payment_id):
   return render_template('payment.html', payment=payment, host=host, url=qr_code_url)
 
 
+@socketio.on('connect')
+def handle_connect():
+  print("Client connected to the server")
+
 if __name__ == '__main__':
   with app.app_context():
     db.create_all()
-  app.run(debug=True)
+  socketio.run(app, debug=True)
